6f3a025c42c76ce15620d3b6ba86f6eb6365ba8d
DonaldButters <70549495+DonaldButters@users.noreply.github.com>
2020-09-12 16:40:10 -0500

    homework commit

--- module3/strings.py
+++ module3/strings.py
@@ -1,5 +1,4 @@
-x = 'Peter Parker in Spiderman'
-x = str.capitalize('Peter Parker in Spiderman')
-print (x)
-word= 'Peter Parker in Spiderman'
-print(word.find('er',1))
+list1 = ['Peter', 'Parker', 'in', 'Spiderman']
+print(list1.index('in',2,4,))
+name = "Peter Parker in Spiderman"
+print(name.isalnum())
